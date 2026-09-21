f48c13ee9869160a3dc6a3fa19be444f345c3634
Micah Sandberg <sandbergmicah@gmail.com>
2026-09-21 00:47:14 -0700

    Add Input FIle

--- Project3.3/Main.c
+++ Project3.3/Main.c
@@ -1,3 +1,19 @@
 #include "Solutions.h"
 
-FILE* File_steam = fopen("temp.txt", "r");
+int Main(void)
+{
+	FILE* Input_File = fopen("Input.txt", "r");
+	FILE* Output_File = fopen("Output.txt", "w");
+
+	double No1 = -1, No2 = -1, No3 = -1, No4 = -1, No5 = -1;
+
+	double read_double(FILE * Input_File);
+	int read_int(FILE * Input_File);
+
+	double No1 = read_double(Input_File), No2 = read_double(Input_File), No3 = read_double(Input_File), No4 = read_double(Input_File), No5 = read_double(Input_File);
+
+
+
+
+	return 0;
+}
