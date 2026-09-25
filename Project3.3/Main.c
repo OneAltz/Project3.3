00e59367dd0dc74ba28502a7f04f53d941c62d7e
Micah Sandberg <sandbergmicah@gmail.com>
2026-09-25 02:39:09 -0700

    Added almost all of main

--- Project3.3/Main.c
+++ Project3.3/Main.c
@@ -1,18 +1,56 @@
 #include "Solutions.h"
 
-int Main(void)
+int main(void)
 {
 	FILE* Input_File = fopen("Input.txt", "r");
 	FILE* Output_File = fopen("Output.txt", "w");
 
-	double No1 = -1, No2 = -1, No3 = -1, No4 = -1, No5 = -1;
+	//Student 1
+	int student_ID1 = read_integer(Input_File);
+	double GPA1 = read_double(Input_File);
+	int class_standing1 = read_integer(Input_File);
+	double age1 = read_double(Input_File);
 
-	double read_double(FILE * Input_File);
-	int read_int(FILE * Input_File);
+	//Student 2
+	int student_ID2 = read_integer(Input_File);
+	double GPA2 = read_double(Input_File);
+	int class_standing2 = read_integer(Input_File);
+	double age2 = read_double(Input_File);
 
-	double No1 = read_double(Input_File), No2 = read_double(Input_File), No3 = read_double(Input_File), No4 = read_double(Input_File), No5 = read_double(Input_File);
+	//Student 3
+	int student_ID3 = read_integer(Input_File);
+	double GPA3 = read_double(Input_File);
+	int class_standing3 = read_integer(Input_File);
+	double age3 = read_double(Input_File);
 
+	//Student 4
+	int student_ID4 = read_integer(Input_File);
+	double GPA4 = read_double(Input_File);
+	int class_standing4 = read_integer(Input_File);
+	double age4 = read_double(Input_File);
 
+	//Student 5
+	int student_ID5 = read_integer(Input_File);
+	double GPA5 = read_double(Input_File);
+	int class_standing5 = read_integer(Input_File);
+	double age5 = read_double(Input_File);
+
+	//Function Calls & storing the results in variables
+	double GPA_Sum = Calculate_Sum(GPA1, GPA2, GPA3, GPA4, GPA5);
+	double GPA_Mean = Calculate_mean(GPA_Sum, 5);
+	double Age_Sum = Calculate_Sum(age1, age2, age3, age4, age5);
+	double Age_Mean = Calculate_mean(Age_Sum, 5);
+	double class_standing_Sum = Calculate_Sum(class_standing1, class_standing2, class_standing3, class_standing4, class_standing5);
+	double class_standing_Mean = Calculate_mean(class_standing_Sum, 5);
+	double GPA_Deviation1 = Calculate_deviation(GPA1, GPA_Mean);
+	double GPA_Deviation2 = Calculate_deviation(GPA2, GPA_Mean);
+	double GPA_Deviation3 = Calculate_deviation(GPA3, GPA_Mean);
+	double GPA_Deviation4 = Calculate_deviation(GPA4, GPA_Mean);
+	double GPA_Deviation5 = Calculate_deviation(GPA5, GPA_Mean);
+	double gpa_variance = Calculate_variance(GPA_Deviation1, GPA_Deviation2, GPA_Deviation3, GPA_Deviation4, GPA_Deviation5, 5);
+	double gpa_standard_deviation = Calculate_standard_deviation(gpa_variance);
+	double maximum_GPA = Find_Maximum(GPA1, GPA2, GPA3, GPA4, GPA5);
+	double minimum_GPA = Find_Minimum(GPA1, GPA2, GPA3, GPA4, GPA5);
 
 
 	return 0;
--- Project3.3/Solutions.h
+++ Project3.3/Solutions.h
@@ -4,18 +4,26 @@
 #include <stdio.h> 
 #include <math.h>
 
-//This Is the Funtion Prototype For No.1
+//Before No.1 - No.7
+//Note: The following function prototypes are for reading and printing data from/to files. These functions are used in the main program to read input data from a file and print output data to a file.
+//STUDY FOR TEST DUMMIE
+double read_double(FILE* infile);
+int read_integer(FILE* infile);
+void print_double(FILE* outfile, double number);
+
+//Function prototypes for the functions used in the main program
+//This Is the Function Prototype For No.1
 double Calculate_Sum(double No1, double No2, double No3, double No4, double No5);
-//This Is the Funtion Prototype For No.2
+//This Is the Function Prototype For No.2
 double Calculate_mean(double Sum, int number);
-//This Is the Funtion Prototype For No.3
+//This Is the Function Prototype For No.3
 double Calculate_deviation(double Number, double Mean);
-//This Is the Funtion Prototype For No.4
+//This Is the Function Prototype For No.4
 double Calculate_variance(double Deviation1, double Deviation2, double Deviation3, double Deviation4, double Deviation5, int number);
-//This Is the Funtion Prototype For No.5
+//This Is the Function Prototype For No.5
 double Calculate_standard_deviation(double Variance);
-//This Is the Funtion Prototype For No.6
+//This Is the Function Prototype For No.6
 double Find_Maximum(double No1, double No2, double No3, double No4, double No5);
-//This Is the Funtion Prototype For No.7
+//This Is the Function Prototype For No.7
 double Find_Minimum(double No1, double No2, double No3, double No4, double No5);
 #endif
